7c1238d51c96e674347f1120278539c74e6f75d6
Rachana1104 <y.rachana1104@gmail.com>
2022-03-12 12:35:55 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './auth/Login';
+import Signup from './auth/Signup';
+import NavbarLogin from './components/Header/NavbarLogin';
+import Navbar from './components/Header/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+      <Router>
+      <Routes>
+        <Route exact path="/" element={<><NavbarLogin/><Signup/></>} />
+        <Route exact path="/login" element={<><NavbarLogin/><Login /></>} />
+        {/* <Route path="/home" element={<><HomePage/></>}></Route>
+        <Route path="/yoga" element={<><Navbar /><Yoga/></>}></Route>
+        <Route path="/meditation" element={<><Navbar /><Meditation/></>}></Route>*/}
+        <Route path="*" element={<><NotFound/></>}></Route> 
+      </Routes>
+    </Router>
     </div>
   );
 }
